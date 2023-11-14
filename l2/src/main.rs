6cb77d60cce21ecade01863141b2db71348596ee
Breadp4ck <iii103@mail.ru>
2023-11-14 20:57:02 +0700

    l2: add supplements, title, order logic

--- l2/src/main.rs
+++ l2/src/main.rs
@@ -1,12 +1,21 @@
 use std::fmt::Display;
 
-use iced::widget::{button, column, container, radio, row, text};
-use iced::{Element, Length, Sandbox, Settings};
+// Библиотека для интерфейса
+use iced::widget::{button, checkbox, column, container, radio, row, text};
+use iced::{Color, Element, Length, Sandbox, Settings};
 
 pub fn main() -> iced::Result {
+    // Интерфейс построен с помощью библиотеки Iced.
+    // Iced использует Elm модель:
+    // - Приложение находится в некотором состоянии (State)
+    // - Пользователь взаимодействут с приложением при помощи сообщений (Messages)
+    // - Для отображения состояний используются виджеты, которые предоставляют
+    //   пользователю возможность создавать сообщения (View logic)
+    // - Сообщения обновляют состояние приложения (Update logic)
     App::run(Settings::default())
 }
 
+// Модель шаурмы
 #[derive(Clone, Copy, Debug, PartialEq, PartialOrd, Eq, Ord)]
 pub enum Kind {
     Classic,
@@ -16,6 +25,7 @@ pub enum Kind {
     Beef,
 }
 
+// Реализация Display для текстового представления модели шаурмы
 impl Display for Kind {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
@@ -28,6 +38,7 @@ impl Display for Kind {
     }
 }
 
+// Размер шаурмы
 #[derive(Clone, Copy, Debug, PartialEq, PartialOrd, Eq, Ord)]
 pub enum Size {
     Small,
@@ -35,6 +46,7 @@ pub enum Size {
     Large,
 }
 
+// Реализация Display для текстового представления размера шаурмы
 impl Display for Size {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
@@ -45,46 +57,86 @@ impl Display for Size {
     }
 }
 
+// Добавки
 #[derive(Clone, Copy, Debug, PartialEq, PartialOrd, Eq, Ord)]
+pub enum Supplements {
+    Cheese,
+    Mushrooms,
+    Jalopenos,
+}
+
+// Шаурма
 pub struct Shaurma {
     kind: Kind,
     size: Size,
+    cheese: bool,
+    mushrooums: bool,
+    jalopenos: bool,
 }
 
 impl Shaurma {
+    // Конструктор шаурмы
     pub fn new(kind: Kind, size: Size) -> Self {
-        Self { kind, size }
-    }
-
-    pub fn price(&self) -> u16 {
-        match (self.kind, self.size) {
-            (Kind::Classic, Size::Small) => 100,
-            (Kind::Classic, Size::Medium) => 200,
-            (Kind::Classic, Size::Large) => 200,
-            (Kind::Cheese, Size::Small) => 200,
-            (Kind::Cheese, Size::Medium) => 100,
-            (Kind::Cheese, Size::Large) => 200,
-            (Kind::Cesar, Size::Small) => 200,
-            (Kind::Cesar, Size::Medium) => 100,
-            (Kind::Cesar, Size::Large) => 200,
-            (Kind::Meat, Size::Small) => 200,
-            (Kind::Meat, Size::Medium) => 100,
-            (Kind::Meat, Size::Large) => 200,
-            (Kind::Beef, Size::Small) => 100,
-            (Kind::Beef, Size::Medium) => 200,
-            (Kind::Beef, Size::Large) => 200,
+        Self {
+            kind,
+            size,
+            cheese: false,
+            mushrooums: false,
+            jalopenos: false,
         }
     }
+
+    // Подсчёт цены для шаурмы
+    pub fn price(&self) -> u32 {
+        let mut price = match (self.kind, self.size) {
+            (Kind::Classic, Size::Small) => 100,
+            (Kind::Classic, Size::Medium) => 140,
+            (Kind::Classic, Size::Large) => 170,
+            (Kind::Cheese, Size::Small) => 130,
+            (Kind::Cheese, Size::Medium) => 160,
+            (Kind::Cheese, Size::Large) => 190,
+            (Kind::Cesar, Size::Small) => 125,
+            (Kind::Cesar, Size::Medium) => 155,
+            (Kind::Cesar, Size::Large) => 185,
+            (Kind::Meat, Size::Small) => 130,
+            (Kind::Meat, Size::Medium) => 160,
+            (Kind::Meat, Size::Large) => 190,
+            (Kind::Beef, Size::Small) => 145,
+            (Kind::Beef, Size::Medium) => 175,
+            (Kind::Beef, Size::Large) => 215,
+        };
+
+        if self.cheese {
+            price += 15;
+        }
+
+        if self.mushrooums {
+            price += 25;
+        }
+
+        if self.jalopenos {
+            price += 20;
+        }
+
+        price
+    }
 }
 
+// Виджет-приложение
 struct App {
     shaurma: Shaurma,
+    ordered: bool,
 }
 
-#[derive(Debug, Clone, Copy)]
+// Возможные сообщения/события в рамках виджета-приложения
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Message {
     SelectKind(Kind),
     SelectSize(Size),
+    AdditionalCheese(bool),
+    AdditionalMushrooms(bool),
+    AdditionalJalopenos(bool),
+    Order,
 }
 
 impl Sandbox for App {
@@ -93,14 +145,20 @@ impl Sandbox for App {
     fn new() -> Self {
         Self {
             shaurma: Shaurma::new(Kind::Classic, Size::Small),
+            ordered: false,
         }
     }
 
     fn title(&self) -> String {
-        String::from("Стекляшка")
+        String::from("Шаурма")
     }
 
+    // Реагирование на сообщения
     fn update(&mut self, message: Message) {
+        if message != Message::Order {
+            self.ordered = false;
+        }
+
         match message {
             Message::SelectKind(kind) => {
                 self.shaurma.kind = kind;
@@ -108,10 +166,24 @@ impl Sandbox for App {
             Message::SelectSize(size) => {
                 self.shaurma.size = size;
             }
+            Message::AdditionalCheese(cheese) => {
+                self.shaurma.cheese = cheese;
+            }
+            Message::AdditionalMushrooms(mushrooms) => {
+                self.shaurma.mushrooums = mushrooms;
+            }
+            Message::AdditionalJalopenos(jalopenos) => {
+                self.shaurma.jalopenos = jalopenos;
+            }
+            Message::Order => {
+                self.ordered = true;
+            }
         }
     }
 
+    // Рисование виджетов
     fn view(&self) -> Element<Message> {
+        // Модель шаурмы
         let select_kind = [
             Kind::Classic,
             Kind::Cesar,
@@ -121,7 +193,7 @@ impl Sandbox for App {
         ]
         .iter()
         .fold(
-            column![text("Выберете шаурму")].spacing(10),
+            column![text("Выберите шаурму")].spacing(10),
             |column, kind| {
                 column.push(radio(
                     format!("{kind}"),
@@ -132,8 +204,9 @@ impl Sandbox for App {
             },
         );
 
+        // Размер шаурмы
         let select_size = [Size::Small, Size::Medium, Size::Large].iter().fold(
-            column![text("Выберете размер")].spacing(10),
+            column![text("Выберите размер")].spacing(10),
             |column, size| {
                 column.push(radio(
                     format!("{size}"),
@@ -144,13 +217,40 @@ impl Sandbox for App {
             },
         );
 
+        // Добавки
+        let select_supplements = column![
+            text("Выберите добавку"),
+            checkbox("Сыр", self.shaurma.cheese, Message::AdditionalCheese),
+            checkbox(
+                "Грибы",
+                self.shaurma.mushrooums,
+                Message::AdditionalMushrooms
+            ),
+            checkbox(
+                "Халопеньо",
+                self.shaurma.jalopenos,
+                Message::AdditionalJalopenos
+            )
+        ]
+        .spacing(10);
+
+        // Итоговая цена шаурмы
         let price = self.shaurma.price().to_string();
 
+        // Кнопка "Заказать" или сообщение о том, что уже заказано
+        let order = if self.ordered {
+            column![text("Шаурма заказана!").size(23.8)] // При 23 и 24 дёргаются на один пиксель разные части интерфейса. Да, это костыль
+        } else {
+            column![button("Заказать!").on_press(Message::Order)]
+        };
+
+        // Сборка элементов интерфейса
         container(
             column![
-                row![select_kind, select_size].spacing(10),
+                text("Шаурма").size(57).style(Color::from([0.15, 0.1, 0.1])),
+                row![select_kind, select_size, select_supplements].spacing(20),
                 text(format!("{price} рублей")),
-                button("Заказать!")
+                order,
             ]
             .align_items(iced::Alignment::Center)
             .spacing(10),
